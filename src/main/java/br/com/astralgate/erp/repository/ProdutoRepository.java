07a7f04f8ab107dcd4d2643a9d5d88482a8d15a6
klaudio0707 <claudioroberto0707@outlook.com>
2026-03-20 11:06:39 -0300

    fix(repository): corrigir imports da entidade produto e classe UUID

--- src/main/java/br/com/astralgate/erp/repository/ProdutoRepository.java
+++ src/main/java/br/com/astralgate/erp/repository/ProdutoRepository.java
@@ -1,7 +1,7 @@
 package br.com.astralgate.erp.repository;
 
-import br.com.astralgate.erp.Produto;
-import org.hibernate.validator.constraints.UUID;
+import java.util.UUID;
+import br.com.astralgate.erp.entity.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProdutoRepository extends JpaRepository<Produto, UUID> {
